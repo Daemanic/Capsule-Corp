affd3656169141de6f9daa9e849f4d04fe1107b3
Whiplash <devesh.shrestha21@gmail.com>
2025-12-05 22:36:58 +0545

    Create scan.py
    This script scans a given IP address or host for open ports and retrieves the service banner if available.

---
+++ scan.py
@@ -0,0 +1,58 @@
+import sys
+import socket
+from IPy import IP
+
+def checkIP(ipadr):
+    try: 
+        IP(ipadr)
+        return str(ipadr)
+    except ValueError:
+        try:
+            host = socket.gethostbyname(ipadr)
+            return host
+        except socket.gaierror:
+            print(f"\n[!] error: invalid")
+            return None
+
+def getBanner(socketValue):
+    return socketValue.recv(1024)
+
+def searchIP(ipadr, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1.0)
+        sock.connect((ipadr, port))
+        try:
+            service = getBanner(sock)
+            print(f"[+] status: [{port}] | [UP] | {service}\n")
+
+            sock.close()
+        except socket.timeout:
+            print(f"[+] status: [{port}] | [NB]\n")
+    except socket.timeout:
+        print(f"[-] status: [{port}] | [TO]\n")
+    except ConnectionRefusedError:
+        print(f"[-] status: [{port}] | [RE]\n")
+    except OSError as e:
+        print(f"[-] status: [{port}] | [OS]\n")
+     
+def scanIP(ipadr):
+    domain = checkIP(ipadr)
+    if domain is None:
+        return
+    print(f"\n[?] target: {domain}\n")
+    port = 22
+    searchIP(domain, port)
+
+
+target = input("\n~ ")
+try:
+    if " " in target:
+        for perIP in target.split(" "):
+            scanIP(perIP)
+    else:
+        scanIP(target)
+
+except KeyboardInterrupt:
+    print("\n[0] executed: ")
+    sys.exit()
