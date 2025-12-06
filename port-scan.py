e5ea01b18963b4c76b59d1d380997ed841143f6d
Whiplash <devesh.shrestha21@gmail.com>
2025-12-06 14:37:04 +0545

    Update port-scan.py

--- port-scan.py
+++ port-scan.py
@@ -11,24 +11,24 @@ def checkIP(ipadr):
             host = socket.gethostbyname(ipadr)
             return host
         except socket.gaierror:
-            print(f"\n[!] error: invalid")
+            print(f"\n[!] syntax-error: [?D]\n")
             return None
-
-def getBanner(socketValue):
-    return socketValue.recv(1024)
 
 def searchIP(ipadr, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1.0)
         sock.connect((ipadr, port))
+        if port == 80:
+            request = "GET / HTTP/1.0\r\nHost: {}\r\n\r\n".format(ipadr)
+            sock.send(request.encode())
         try:
-            service = getBanner(sock)
-            print(f"[+] status: [{port}] | [UP] | {service.decode()}\n")
-
-            sock.close()
+            service = sock.recv(1024)
+            status = service.decode().split('\n')[0]
+            print(f"[+] status: [{port}] | [UP] {status}\n")
         except socket.timeout:
             print(f"[+] status: [{port}] | [NB]\n")
+        sock.close()
     except socket.timeout:
         print(f"[-] status: [{port}] | [TO]\n")
     except ConnectionRefusedError:
@@ -40,19 +40,26 @@ def scanIP(ipadr):
     domain = checkIP(ipadr)
     if domain is None:
         return
-    print(f"\n[?] target: {domain}\n")
-    for port in range(0,521):
-        searchIP(domain, port)
+    port = input("[?] port: ")
+    print(f"[?] target: {domain}\n")
+    for endPort in port.split():
+        try:
+            if 0 < int(endPort) < 65536:
+                searchIP(domain, int(endPort))
+            else:
+                print(f"\n[!] end-port: [?P]\n")
+        except ValueError:
+            print(f"\n[!] syntax-error: [?D]\n")
 
 if __name__ == "__main__":
-    target = input("\n~ ")
     try:
-        if " " in target:
-            for perIP in target.split(" "):
-                scanIP(perIP)
-        else:
-            scanIP(target)
-    
+        target = input("\n[?] domain: ")
     except KeyboardInterrupt:
-        print("\n[0] executed: ")
+        print(f"\n[0]")
+        sys.exit()
+    try:
+        for perIP in target.split():
+            scanIP(perIP)
+    except KeyboardInterrupt:
+        print(f"\n[0]")
         sys.exit()
