530ce9d266e6f9dd5e024b4ea91f7db404f411b9
Whiplash <devesh.shrestha21@gmail.com>
2026-03-28 01:29:01 +0545

    Update netscan.py

--- Network-Scan/netscan.py
+++ Network-Scan/netscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import scapy.all as scapy
+import prettytable
 
 def program():
     parse = argparse.ArgumentParser()
@@ -12,7 +13,10 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = broadcast/request
     receivedList = scapy.srp(packet, verbose=False, timeout=1)[0]
-    print(receivedList.summary())
+    table = prettytable.PrettyTable(["IP", "MAC Address"])
+    for index in receivedList:
+        table.add_row([index[1].psrc, index[1].src])
+    print(table)
 
 args = program()
 scan(args.ip)
