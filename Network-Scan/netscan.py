685f4f547ad4ea732fc8b15e8f40b52c0971d697
Whiplash <devesh.shrestha21@gmail.com>
2026-03-28 00:53:24 +0545

    Update netscan.py

--- Network-Scan/netscan.py
+++ Network-Scan/netscan.py
@@ -4,14 +4,14 @@ import scapy.all as scapy
 
 def program():
     parse = argparse.ArgumentParser()
-    parse.add_argument("-i", "--ipadr", required=True, help="enter local network gateway")
+    parse.add_argument("-i", "--ipadr", dest="ip", required=True, help="enter local network ip-address")
     return parse.parse_args()
 
 def scan(ip):
     request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = broadcast/request
-    receivedList, unused = scapy.srp(packet, verbose=False, timeout=1)
+    receivedList = scapy.srp(packet, verbose=False, timeout=1)[0]
     print(receivedList.summary())
 
 args = program()
