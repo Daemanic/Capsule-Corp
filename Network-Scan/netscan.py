1c1db79191dde546ca1c4676762a5f31d53eb29b
Whiplash <devesh.shrestha21@gmail.com>
2026-03-28 00:24:57 +0545

    Update netscan.py

--- Network-Scan/netscan.py
+++ Network-Scan/netscan.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import scapy.all as scapy
+import optparse
+
+def program():
+    parse = optparse.OptionParser()
+    parse.add_option("-i", "--ipadr", dest="ip", help="enter local network gateway")
+    (option, argument) = parse.parse_args()
+    if not option.ip:
+        parse.error("use -h or --help")
+    return option
+
+def scan(ip):
+    request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast/request
+    receivedList, unused = scapy.srp(packet, verbose=False, timeout=1)
+    print(receivedList.summary())
+
+option = program()
+scan(option.ip)
